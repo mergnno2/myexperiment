da78632ef4665c3ef9abb5832e4c9689447aee32
Supernova <3174431923@qq.com>
2021-09-10 21:39:30 +0800

    Read data process complete.

---
+++ portScanningDetection/Network_info.py
@@ -0,0 +1,70 @@
+# This code intend to get the information about the host and their opening ports from CIDDS-001-Internal data set.
+
+import pandas as pd
+import time
+import numpy as np
+import csv
+
+def get_time(time_string):
+    timeArray = time.strptime(time_string, "%Y-%m-%d %H:%M:%S.%f")
+    timeStamp = float(time.mktime(timeArray))
+    return timeStamp
+
+filepath = "D:\Python\Python37\myexperiment\portScanningDetection\CIDDS-001\\traffic\OpenStack\CIDDS-001-internal-week1.csv"
+# open the original csv data file
+file = csv.reader(open(filepath, 'r'))
+
+start_bound = 0
+end_bound = 5
+
+timing = 0
+
+network_info = {}
+isFirstrow = True
+head = next(file)
+firstrow = next(file)
+start = get_time(firstrow[0])
+window_index=0
+
+for row in file:
+
+    if isFirstrow:
+        isFirstrow = False
+
+    end = get_time(row[0])
+    timeArray = time.strptime(row[0], "%Y-%m-%d %H:%M:%S.%f")
+    if timeArray.tm_hour > timing:
+        print(str(timeArray.tm_hour) + "o'clock")
+        timing = timing + 1
+    elif timeArray.tm_hour < start_bound:
+        continue
+    elif timeArray.tm_hour >= end_bound:
+        # Calculate precision here.
+
+        items=network_info.items()
+        with open('Network_Information.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            for item in items:
+                l=[]
+                l.append(item[0])
+                r=l+item[1]
+                writer.writerow(r)
+
+        #pd.DataFrame(network_info).to_csv("Network_Information.csv")
+        exit(0)
+    if row[12] != "normal":
+        continue
+    IP = row[3]
+    port = row[4]
+    result = network_info.get(IP)
+    if result == None:
+        ports = []
+        ports.append(port)
+        item = {IP:ports}
+        network_info.update(item)
+    else:
+        if port in result:
+            continue
+        else:
+            result.append(port)
+            network_info[IP]=result
--- portScanningDetection/SequentialTest.py
+++ portScanningDetection/SequentialTest.py
@@ -7,15 +7,61 @@ import time
 import numpy as np
 import csv
 
+class Host(object):
+    def __init__(self,IP):
+        self.IP=IP
+        self.opening_ports=[]
+
+class Network_Event(object):
+    def __init__(self,IP):
+        self.IP=IP
+        self.ICMP=0
+        self.RST=0
+        self.RwA=0
+        self.NeIP=0
+        self.NeTCP=0
+    def calculate_attribute(self,timewindow):
+        ICMP = RST = RwA = NeIP = NeTCP = 0
+
+        self.ICMP=ICMP
+        self.RST=RST
+        self.RwA=RwA
+        self.NeIP=NeIP
+        self.NeTCP=NeTCP
+
 def get_time(time_string):
     timeArray = time.strptime(time_string, "%Y-%m-%d %H:%M:%S.%f")
     timeStamp = float(time.mktime(timeArray))
     return timeStamp
 
+def check_IP(IP):
+    for event in network_events:
+        if event.IP == IP:
+            return False
+    return True
+
+def generate_network_event(time_window):
+    network_events.clear()
+
+    isNewIP = False
+    for eachflow in time_window:
+        IP = eachflow[3]
+        isNewIP = check_IP(IP)
+        if isNewIP:
+            event = Network_Event(IP)
+            isNewIP = False
+        else:
+            pass
+
+    return
+information_path = "D:\Python\Python37\myexperiment\portScanningDetection\\Network_Information.csv"
 filepath = "D:\Python\Python37\myexperiment\portScanningDetection\CIDDS-001\\traffic\OpenStack\CIDDS-001-internal-week1.csv"
 # open the original csv data file
+info = csv.reader(open(information_path, 'r'))
 file = csv.reader(open(filepath, 'r'))
 
+network_info = {}
+network_events = []
 flow_data = []
 start_bound = 0
 end_bound = 9
@@ -27,6 +73,17 @@ head = next(file)
 firstrow = next(file)
 start = get_time(firstrow[0])
 window_index=0
+
+
+for row in info:
+    i = 1
+    value = []
+    while i <len(row):
+        value.append(row[i])
+        i = i + 1
+    item = {row[0]:value}
+    network_info.update(item)
+print("hello")
 
 for row in file:
 
@@ -49,6 +106,7 @@ for row in file:
     flow_data[window_index].append(row)
 
     if end - start >= 60:
+        generate_network_event(flow_data[window_index])
         start = end
         flow_data.append([])
         window_index = window_index + 1
