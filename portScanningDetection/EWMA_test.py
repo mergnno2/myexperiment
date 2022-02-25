05034dafa82df81bc8a9ea27701d12836cd6d37e
mergnno2 <75164499+mergnno2@users.noreply.github.com>
2022-02-25 21:21:56 +0800

    It's an simple test for the functionality of EWMA.

---
+++ portScanningDetection/EWMA_test.py
@@ -0,0 +1,17 @@
+# this file aims to test the EWMA algorithm using simple sequence of digital number.
+import random
+import matplotlib.pyplot as plt
+
+numbers = []
+ewma = []
+numbers.append(0)
+ewma.append(0)
+alpha = 0.2
+for i in range(1, 200):
+    numbers.append(random.randint(numbers[i - 1] - 5, numbers[i - 1] + 5))
+    ewma.append((1 - alpha) * ewma[i - 1] + alpha * numbers[i])
+plt.plot(numbers)
+plt.plot(ewma)
+print(numbers)
+print(ewma)
+plt.show()
